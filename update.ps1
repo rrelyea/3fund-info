3a144736cc26e04c314e66f5881400787828e837
Rob Relyea <rob@relyeas.net>
2021-07-06 16:07:15 -0700

    repeat command every 3 minutes

--- update.ps1
+++ update.ps1
@@ -1,5 +1,9 @@
-msbuild src\daily.csproj -p:configuration=release -v:m
-& .\src\bin\release\net5.0\daily.exe
-& git add -A
-& git commit -m "price update"
-& git push
+while ($true)
+{
+    msbuild src\daily.csproj -p:configuration=release -v:m
+    & .\src\bin\release\net5.0\daily.exe
+    & git add -A
+    & git commit -m "price update"
+    & git push
+    start-sleep -seconds 180
+}
