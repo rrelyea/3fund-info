8e3462427920384b5f7a0bf180ba5a9e98595ae5
Rob Relyea <rob@relyeas.net>
2021-07-19 13:55:46 -0700

    overwrite html files; fix git add path

--- update.ps1
+++ update.ps1
@@ -2,9 +2,9 @@
 & git add -A perf\*
 & git commit -m "price update"
 & git push
-Copy-Item -Path perf\ -Filter *.html -Destination ..\rrelyea.github.io\3fund\ -Recurse
+Copy-Item -Path perf\ -Filter *.html -Destination ..\rrelyea.github.io\3fund\ -Recurse -Force
 & cd ..\rrelyea.github.io
-& git add ..\3fund\*.html
+& git add 3fund\*.html
 & git commit -m "price update"
 & git push
 & cd ..\3fund-info
